37ff9671df4acd8f96499a446d9590f8a7e1f5db
conorburke <cjburke89@gmail.com>
2017-01-05 17:39:20 -0800

    render basic message to browser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-require { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <div>Telescopes are Time Machines</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
