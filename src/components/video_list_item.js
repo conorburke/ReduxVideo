6a7fde8827add5673414cf24620a9f6b85721c72
conorburke <cjburke89@gmail.com>
2017-01-08 15:51:42 -0800

    show video list with thumbnail image and title

---
+++ src/components/video_list_item.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+  // const video = props.video  is the same as having props in the arrow function and method to the left
+  console.log(video)
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl}/>
+        </div>
+        <div className="media-body">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+      </div>
+    </li>
+  )
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDZ57pcmkRwqo3Rei23l3ZwFDr9aKpv7i4';
 
-const App = () => {
-  return (
-    <div>
-      <h1 style={{ color: 'blue' }}>Telescopes are Time Machines</h1>
-      <SearchBar />
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 'videos': [] };
+  
+    YTSearch({ key: API_KEY, term: 'telescopes' }, (videos) => {
+      this.setState({ videos }); //es6 syntax equal to { 'videos': videos }
+    })
+
+  }
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: 'blue' }}>Telescopes are Time Machines</h1>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
