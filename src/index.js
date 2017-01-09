6b845002966f01c276d01955c2ce1e45b9f38c12
conorburke <cjburke89@gmail.com>
2017-01-08 16:31:04 -0800

    enable user to select and watch videos from playlist

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDZ57pcmkRwqo3Rei23l3ZwFDr9aKpv7i4';
 
@@ -11,10 +12,16 @@ const API_KEY = 'AIzaSyDZ57pcmkRwqo3Rei23l3ZwFDr9aKpv7i4';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 'videos': [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+     };
   
     YTSearch({ key: API_KEY, term: 'telescopes' }, (videos) => {
-      this.setState({ videos }); //es6 syntax equal to { 'videos': videos }
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
     })
 
   }
@@ -23,7 +30,10 @@ class App extends Component {
       <div>
         <h1 style={{ color: 'blue' }}>Telescopes are Time Machines</h1>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
